e0861d7821e1b53cd5c4be1d06e981aa220b65f0
justerest <justerest@gmail.com>
2022-09-21 15:20:23 +0500

    feat: first user with address

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,56 @@
+import { PrismaClient, StringAddress } from '@prisma/client';
 import { Telegraf } from 'telegraf';
 
 import { BOT_TOKEN } from './env';
 
+const prisma = new PrismaClient();
 const bot = new Telegraf(BOT_TOKEN);
 
 const shouldSetAddress: Set<number> = new Set();
-const addressMap: Map<number, string> = new Map();
 
-bot.start((ctx) => ctx.reply('Добро пожаловать!\n Введите адрес'));
-bot.help((ctx) => ctx.reply('Not implemented'));
+enum BotCommand {
+  GetAddress = 'getaddress',
+  SetAddress = 'setaddress',
+}
 
-bot.command('setaddress', (ctx) => {
+bot.start(async (ctx) => {
+  await prisma.user.create({ data: ctx.from });
+
+  ctx.reply('Добро пожаловать!');
+});
+
+bot.help((ctx) => ctx.reply('Укажите адрес, передавайте показания по запросу бота.'));
+
+bot.command(BotCommand.SetAddress, (ctx) => {
   shouldSetAddress.add(ctx.chat.id);
   ctx.reply('Введите адрес');
 });
 
-bot.command('getaddress', (ctx) => {
-  const address = addressMap.get(ctx.chat.id);
+bot.command(BotCommand.GetAddress, async (ctx) => {
+  const address = await prisma.stringAddress.findUnique({ where: { userId: ctx.from.id } });
+
   if (address) {
-    ctx.reply(`Ваш адрес: ${address}`);
+    ctx.reply(`Ваш адрес: ${address.value}`);
   } else {
     ctx.reply('Вы ещё не указали адрес');
   }
 });
 
-bot.on('text', (ctx) => {
+bot.on('text', async (ctx) => {
+  console.log(ctx.message.text);
+
   if (shouldSetAddress.has(ctx.chat.id)) {
     shouldSetAddress.delete(ctx.chat.id);
-    console.log(ctx.message.text);
-    addressMap.set(ctx.chat.id, ctx.message.text);
+
+    const userId = ctx.from.id;
+    const addressRecord: StringAddress = { userId, value: ctx.message.text };
+
+    await prisma.stringAddress.upsert({
+      where: { userId },
+      create: addressRecord,
+      update: addressRecord,
+    });
+
     ctx.reply('Адрес сохранен');
   }
 });
@@ -36,5 +58,12 @@ bot.on('text', (ctx) => {
 bot.launch();
 
 // Enable graceful stop
-process.once('SIGINT', () => bot.stop('SIGINT'));
-process.once('SIGTERM', () => bot.stop('SIGTERM'));
+process.once('SIGINT', async () => {
+  bot.stop('SIGINT');
+  await prisma.$disconnect();
+});
+
+process.once('SIGTERM', async () => {
+  bot.stop('SIGTERM');
+  await prisma.$disconnect();
+});

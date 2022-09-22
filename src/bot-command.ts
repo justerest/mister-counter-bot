88e3054895d3e117bf3217974b050ac52c65809f
justerest <justerest@gmail.com>
2022-09-22 11:14:11 +0500

    feat: set address scene

---
+++ src/bot-command.ts
@@ -0,0 +1,5 @@
+export enum BotCommand {
+  GetAddress = 'get_address',
+  SetAddress = 'set_address',
+  LogWater = 'log_water',
+}
--- src/main.ts
+++ src/main.ts
@@ -1,40 +1,40 @@
-import { PrismaClient, User } from '@prisma/client';
-import { Telegraf } from 'telegraf';
+import { User } from '@prisma/client';
+import { Scenes, session, Telegraf } from 'telegraf';
 
+import { BotCommand } from './bot-command';
 import { BOT_TOKEN } from './env';
+import { prisma } from './prisma';
+import { SET_ADDRESS_SCENE, setAddressScene } from './set-address-scene';
 
-const prisma = new PrismaClient();
-const bot = new Telegraf(BOT_TOKEN);
+const bot = new Telegraf<Scenes.SceneContext>(BOT_TOKEN);
 
-const userSessionMap: Map<number, UserSessionState> = new Map();
-
-enum BotCommand {
-  GetAddress = 'getaddress',
-  SetAddress = 'setaddress',
-}
-
-enum UserSessionState {
-  Default = 'Default',
-  SettingAddress = 'SettingAddress',
-}
+bot.use(session());
+bot.use(new Scenes.Stage([setAddressScene()]).middleware());
 
 bot.start(async (ctx) => {
-  await prisma.user.upsert({
+  const user: Omit<User, 'address'> = {
+    id: ctx.from.id,
+    first_name: ctx.from.first_name,
+    last_name: ctx.from.last_name ?? null,
+    username: ctx.from.username ?? null,
+  };
+
+  const savedUser = await prisma.user.upsert({
     where: { id: ctx.from.id },
-    create: ctx.from,
-    update: ctx.from,
+    create: user,
+    update: user,
   });
 
-  ctx.reply('Добро пожаловать!');
+  await ctx.reply(`Здравствуйте, ${ctx.from.first_name}!`);
+
+  if (!savedUser.address) {
+    await ctx.scene.enter(SET_ADDRESS_SCENE);
+  }
 });
 
 bot.help((ctx) => ctx.reply('Укажите адрес, передавайте показания по запросу бота.'));
 
-bot.command(BotCommand.SetAddress, async (ctx) => {
-  userSessionMap.set(ctx.from.id, UserSessionState.SettingAddress);
-
-  await ctx.reply('Введите адрес');
-});
+bot.command(BotCommand.SetAddress, Scenes.Stage.enter<Scenes.SceneContext>(SET_ADDRESS_SCENE));
 
 bot.command(BotCommand.GetAddress, async (ctx) => {
   const user = await prisma.user.findUnique({ where: { id: ctx.from.id } });
@@ -42,33 +42,7 @@ bot.command(BotCommand.GetAddress, async (ctx) => {
   if (user?.address) {
     await ctx.reply(`Ваш адрес: ${user.address}`);
   } else {
-    await ctx.reply('Вы ещё не указали адрес');
-  }
-});
-
-bot.on('text', async (ctx) => {
-  console.log(ctx.message.text);
-
-  const userSessionState = userSessionMap.get(ctx.chat.id);
-
-  if (userSessionState === UserSessionState.SettingAddress) {
-    const user: User = {
-      id: ctx.from.id,
-      first_name: ctx.from.first_name,
-      last_name: ctx.from.last_name ?? null,
-      username: ctx.from.username ?? null,
-      address: ctx.message.text,
-    };
-
-    await prisma.user.upsert({
-      where: { id: ctx.from.id },
-      create: user,
-      update: user,
-    });
-
-    userSessionMap.set(ctx.from.id, UserSessionState.Default);
-
-    await ctx.reply('Ваш адрес сохранен');
+    await ctx.scene.enter(SET_ADDRESS_SCENE);
   }
 });
 
